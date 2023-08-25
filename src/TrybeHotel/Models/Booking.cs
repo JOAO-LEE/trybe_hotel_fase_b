5eec21bab7d898622da4c3815ddf89449438e409
João Lee <joaumlimaum@gmail.com>
2023-08-25 15:51:20 -0300

    Corrige model de Booking

--- src/TrybeHotel/Models/Booking.cs
+++ src/TrybeHotel/Models/Booking.cs
@@ -6,8 +6,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class Booking
 {
   public int BookingId { get; set; }
-  public int CheckIn { get; set; }
-  public int CheckOut { get; set; }
+  public DateTime CheckIn { get; set; }
+  public DateTime CheckOut { get; set; }
   public int GuestQuant { get; set; }
   public int UserId { get; set; }
   public int RoomId { get; set; }
