82dca7d9784dfba9420f2c3d22d37cecd97090e9
João Lee <joaumlimaum@gmail.com>
2023-08-25 15:32:23 -0300

    Cópia/implementação parcial do contexto

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,13 +4,21 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
-    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) {
+    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options)
+    {
         Seeder.SeedUserAdmin(this);
     }
     public TrybeHotelContext() { }
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {}
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) {}
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) { }
 
 }
