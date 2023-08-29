d018a2ab5cc6818492ead7c08049d753e5c8695c
João Lee <joaumlimaum@gmail.com>
2023-08-29 13:54:32 -0300

    Requisito 7 - passando em um dos testes

--- src/TrybeHotel/Controllers/BookingController.cs
+++ src/TrybeHotel/Controllers/BookingController.cs
@@ -11,7 +11,7 @@ namespace TrybeHotel.Controllers
 {
     [ApiController]
     [Route("booking")]
-  
+
     public class BookingController : Controller
     {
         private readonly IBookingRepository _repository;
@@ -21,14 +21,26 @@ namespace TrybeHotel.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add([FromBody] BookingDtoInsert bookingInsert){
-            throw new NotImplementedException();
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Authorize(Policy = "Client")]
+        public IActionResult Add([FromBody] BookingDtoInsert bookingInsert)
+        {
+            var token = HttpContext.User.Identity as ClaimsIdentity;
+            var email = token?.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+            var foundRoom = _repository.GetRoomById(bookingInsert.RoomId);
+            if (foundRoom.Capacity < bookingInsert.GuestQuant)
+            {
+                return BadRequest(new { message = "Guest quantity over room capacity" });
+            }
+            var bookingSchedule = _repository.Add(bookingInsert, email);
+            return Created("", bookingSchedule);
         }
 
 
         [HttpGet("{Bookingid}")]
-        public IActionResult GetBooking(int Bookingid){
-           throw new NotImplementedException();
+        public IActionResult GetBooking(int Bookingid)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- src/TrybeHotel/Repository/BookingRepository.cs
+++ src/TrybeHotel/Repository/BookingRepository.cs
@@ -13,8 +13,34 @@ namespace TrybeHotel.Repository
 
         public BookingResponse Add(BookingDtoInsert booking, string email)
         {
-            throw new NotImplementedException();
+            var foundUser = _context.Users.First(e => e.Email == email);
+            Booking bookingSchedule = new()
+            {
+                UserId = foundUser.UserId,
+                GuestQuant = booking.GuestQuant,
+                CheckIn = booking.CheckIn,
+                CheckOut = booking.CheckOut,
+                RoomId = booking.RoomId
+            };
+            _context.Bookings.Add(bookingSchedule);
+            _context.SaveChanges();
+            return (from bookings in _context.Bookings
+                    where bookings.RoomId == bookingSchedule.RoomId
+                    join rooms in _context.Rooms on bookings.RoomId equals rooms.RoomId
+                    join hotel in _context.Hotels on rooms.HotelId equals hotel.HotelId
+                    join city in _context.Cities on hotel.CityId equals city.CityId
+                    select new BookingResponse
+                    {
+                        BookingId = bookings.BookingId,
+                        CheckIn = bookings.CheckIn,
+                        CheckOut = bookings.CheckOut,
+                        GuestQuant = bookings.GuestQuant,
+                        Room = new RoomDto { RoomId = rooms.RoomId, Capacity = rooms.Capacity, Name = rooms.Name, Image = rooms.Image, Hotel = new HotelDto { HotelId = hotel.HotelId, Address = hotel.Address, Name = hotel.Name, CityId = city.CityId, CityName = city.Name } }
+                    }).ToList().First();
         }
+
+        //  join hotel in _context.Hotels on room.HotelId equals hotel.HotelId
+        //             join city in _context.Cities on hotel.CityId equals city.CityId
 
         public BookingResponse GetBooking(int bookingId, string email)
         {
@@ -23,7 +49,12 @@ namespace TrybeHotel.Repository
 
         public Room GetRoomById(int RoomId)
         {
-            throw new NotImplementedException();
+            var roomToSchedule = _context.Rooms.Find(RoomId);
+            if (roomToSchedule == null)
+            {
+                return null;
+            }
+            return roomToSchedule;
         }
 
     }
